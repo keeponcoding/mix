338a41b949dc072a977caf8965769d43a3aaf74e
cll <chenleilei_92@163.com>
2019-12-29 11:15:28 +0800

    jvm

--- src/main/java/com/cll/mix/jvm/JVMApp.java
+++ src/main/java/com/cll/mix/jvm/JVMApp.java
@@ -25,5 +25,48 @@ public class JVMApp {
 
         System.out.println(s1.equals(s2));// true 比较的是值
 
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        ////////////////////////////////////////////////////////////////////////////////////////
+
+        String t1 = "hello";
+        String t2 = "world";
+        String t3 = "hello" + "world";
+        String t4 = t1 + t2; // new
+        String t5 = "helloworld";
+        String t6 = t4.intern();
+
+        // == 比较的是引用地址
+        System.out.println("t3 == t4 : " + (t3 == t4)); // false
+        System.out.println("t3 == t5 : " + (t3 == t5)); // true
+        System.out.println("t4 == t5 : " + (t4 == t5)); // false
+
+        /*
+         * It follows that for any two strings {@code s} and {@code t},
+         * {@code s.intern() == t.intern()} is {@code true}
+         * if and only if {@code s.equals(t)} is {@code true}.
+         *
+         * intern 当 equals 为true  那么 == 也为true
+         */
+        System.out.println("t3 == t6 : " + (t3.equals(t6))); // true
+        System.out.println("t3 == t6 : " + (t3 == t6)); // true
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        ////////////////////////////////////////////////////////////////////////////////////////
+
+        Integer i1 = 50;
+        Integer i2 = 50;
+        System.out.println("i1 == i2 : " + (i1 == i2)); // true
+
+        Integer i3 = 150;
+        Integer i4 = 150;
+        System.out.println("i3 == i4 : " + (i3 == i4)); // false  -128~127
+
+        Integer i5 = new Integer(50);
+        Integer i6 = new Integer(50);
+        System.out.println("i5 == i6 : " + (i5 == i6)); // false
+
+        Double d1 = 10.1D;
+        Double d2 = 10.1D;
+        System.out.println("d1 == d2 : " + (d1 == d2)); // false
     }
 }
