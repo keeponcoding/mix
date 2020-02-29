ab5ea95bc730ead66c1241507c40f095a7a4fb88
cll <chenleilei_92@163.com>
2020-02-29 10:24:35 +0800

    mix:修改注释

---
+++ src/main/java/com/cll/mix/algorithm/FibNum.java
@@ -0,0 +1,60 @@
+package com.cll.mix.algorithm;
+
+/**
+ * @ClassName FibNum
+ * @Description
+ *
+ * //斐波那契数，通常用 F(n) 表示，形成的序列称为斐波那契数列。
+ * 该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是：
+ * //
+ * // F(0) = 0,   F(1) = 1
+ * //F(N) = F(N - 1) + F(N - 2), 其中 N > 1.
+ * //
+ * // 给定 N，计算 F(N)。
+ * //
+ * // 示例 1：
+ * // 输入：2
+ * //输出：1
+ * //解释：F(2) = F(1) + F(0) = 1 + 0 = 1.
+ * //
+ * // 示例 2：
+ * // 输入：3
+ * //输出：2
+ * //解释：F(3) = F(2) + F(1) = 1 + 1 = 2.
+ * //
+ * //
+ * // 示例 3：
+ * // 输入：4
+ * //输出：3
+ * //解释：F(4) = F(3) + F(2) = 2 + 1 = 3.
+ * //
+ * // 提示：
+ * // 0 ≤ N ≤ 30
+ *
+ * @Author cll
+ * @Date 2020/2/29 9:22 上午
+ * @Version 1.0
+ **/
+public class FibNum {
+
+    public static void main(String[] args) {
+        System.out.println(fib(4));
+    }
+
+    public static int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+
+        int f0 = 0;
+        int f1 = 1;
+        int f2 = 1;
+
+        for (int i = 2; i <= n; i++) {
+            f2 = f1 + f0;
+            f0 = f1;
+            f1 = f2;
+        }
+        return f2;
+    }
+
+}
